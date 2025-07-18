b663ea630d6b9313513b4a9c055c6e39625517de
elmo <potato95101@gmail.com>
2025-07-18 15:43:54 +0000

    chore: 設定 DRF DefaultRouter 並加入基本路由結構至 urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+# from school.views import TeacherViewSet, StudentViewset
+
+router = DefaultRouter(trailing_slash=False)
+# router.register(r'teachers', TeacherViewSet, basename = 'teacher')
+# router.register(r'students', StudentViewset, basename = 'student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls))
 ]
