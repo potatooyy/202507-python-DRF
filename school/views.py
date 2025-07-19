1df751fd6f4cc30c097c4bd5be7d46c3b1ffa20e
elmo <potato95101@gmail.com>
2025-07-19 13:11:49 +0000

    feat(views): add create method for Teacher and Student ViewSets

--- school/views.py
+++ school/views.py
@@ -1,13 +1,32 @@
 # school/views.py
-# from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Teacher, Student
 from .serializers import TeacherSerializer, TeacherSimpleSerializer, StudentSerializer, StudentSimpleSerializer
+
 # Create your views here.
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
 
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
